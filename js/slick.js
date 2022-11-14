b5e9debb336b9575df78da6cad6c87565c05c0e1
Andy <vu.ngoc@news.com.au>
2022-11-14 17:25:47 +0700

    11/14

--- js/slick.js
+++ js/slick.js
@@ -4,3 +4,15 @@ $('.banner__slide').slick({
     prevArrow:"<button type='button' class='slick-prev pull-left banner-prev'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
     nextArrow:"<button type='button' class='slick-next pull-right banner-next'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
 });
+
+
+// Product Carousel
+
+$('.new-products__lists').slick({
+    autoplay: false,
+    infinite: false,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    prevArrow:"<button type='button' class='slick-prev pull-left product-prev'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow:"<button type='button' class='slick-next pull-right product-next'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+});
